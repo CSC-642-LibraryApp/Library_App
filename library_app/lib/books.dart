cb07e6cfef45f03e74b8f7934d5bfc32c726810d
Yunhao Wang <myhwang43@gmail.com>
2021-12-04 10:50:30 -0800

    Minor change

---
+++ library_app/lib/books.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+//import 'package:login_signup_screen/signup.dart';
+
+class Books {
+  String name;
+  String author;
+
+  Books({
+    required this.name,
+    required this.author,
+  });
+}
