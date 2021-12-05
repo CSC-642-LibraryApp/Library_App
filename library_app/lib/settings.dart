104876fb6d1d4fd52315d83560f454a09dcb02e8
Allison Adad <42702624+adadallison@users.noreply.github.com>
2021-12-04 23:28:15 -0800

    Add files via upload
    added main, home,book,wishlist,settings,nav

---
+++ library_app/lib/settings.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+//Minseon
+
+class setting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("Library Lookup"),
+      //   backgroundColor: Color(0xFF112D4E),
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            "My Account",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF112D4E),
+                fontSize: 28),
+          ),
+
+          // Column(
+          //   children: <Widget>[
+          //     _buildButtonColumn("Account Information"),
+          //     _buildButtonColumn(" Books Checked out "),
+          //     _buildButtonColumn("      Your Reviews      ")
+          //   ],
+          // ),
+          Container(
+            child: _buildButtonColumn("Account Information"),
+          ),
+          Container(
+            child: _buildButtonColumn(" Books Checked out "),
+          ),
+          Container(
+            child: _buildButtonColumn("      Your Reviews      "),
+          ),
+          Container(),
+          const Text(
+            "Community",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF112D4E),
+                fontSize: 28),
+          ),
+          _buildButtonColumn("  Lend Your Books  "),
+          const Text(
+            "Support",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF112D4E),
+                fontSize: 28),
+          ),
+          _buildButtonColumn("          Get Help          "),
+        ],
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(
+    String text,
+  ) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            onPrimary: Colors.black,
+            primary: Color(0xFFD3E2F7),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            elevation: 3.0,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
